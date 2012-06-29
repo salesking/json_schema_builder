f21cc3af781bba5c99deb4ccd59ecb34b307916d
Georg Leciejewski <gl@salesking.eu>
2012-06-30 00:51:25 +0200

    require active_record

--- lib/json_schema_builder.rb
+++ lib/json_schema_builder.rb
@@ -1,5 +1,5 @@
 require 'json'
-require "json_schema_builder/version"
-require "json_schema_builder/writer"
+require 'json_schema_builder/version'
+require 'json_schema_builder/writer'
 
 module JsonSchemaBuilder;end
--- lib/json_schema_builder/writer.rb
+++ lib/json_schema_builder/writer.rb
@@ -1,3 +1,4 @@
+require 'active_record'
 module JsonSchemaBuilder
 
   # Create json schema files for each model in
