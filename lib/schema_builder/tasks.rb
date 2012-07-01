97bb848d7450ca4d76bf2e2a7fa9fed603740ba9
Georg Leciejewski <gl@salesking.eu>
2012-07-01 13:20:41 +0200

    fix rake task

--- lib/schema_builder/tasks.rb
+++ lib/schema_builder/tasks.rb
@@ -4,10 +4,9 @@ namespace :schema do
   task :build do |t,args|
     if defined?(Rails) && Rails.respond_to?(:application)
       Rails.application.initialize!
-    elseif defined?(Rails::Initializer)
-      # Rails 2.3
+    else
       puts "== Hiccup ==\n"
-      puts "Sorry Rails < 3 is not supported\n You can still write a schema by hand .. it's not that difficult.\n Btw. you should move to Rails 3 anyway!"
+      puts "Sorry cannot find a Rails 3.+ app.\n You can still write a schema by hand .. it's not that difficult.\n Btw. you should move to Rails 3 anyway!"
       next
     end
     builder = SchemaBuilder::Writer.new
